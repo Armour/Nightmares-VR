37430f81f93783b0f0c44b3c674772bc3a203ced
Armour <497052684@qq.com>
2015-12-12 09:18:34 -0800

    added followed camera

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform target;
+	public float smoothing = 5.0f;
+
+	Vector3 offset;
+
+	void Start() {
+		offset = transform.position - target.position;	
+	}
+
+	void FixedUpdate() {
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
