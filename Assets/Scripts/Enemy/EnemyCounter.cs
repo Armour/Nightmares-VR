feb21056764a661f4a90c99822526e861495a9df
Armour <497052684@qq.com>
2015-12-27 00:05:52 -0800

    added EnemyCounter && added the maximal number of different kinds of emeny

---
+++ Assets/Scripts/Enemy/EnemyCounter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCounter : MonoBehaviour {
+
+	public int maxBunnyNumber;
+	public int maxBearNumber;
+	public int maxElepNumber;
+
+	[HideInInspector] public int bunnyNumber = 0;
+	[HideInInspector] public int bearNumber = 0;
+	[HideInInspector] public int elepNumber = 0;
+
+	public void increaseBunny() {
+		bunnyNumber++;
+	}
+
+	public void increaseBear() {
+		bearNumber++;
+	}
+
+	public void increaseElep() {
+		elepNumber++;
+	}
+
+	public void decreaseBunny() {
+		bunnyNumber--;
+	}
+
+	public void decreaseBear() {
+		bearNumber--;
+	}
+
+	public void decreaseElep() {
+		elepNumber--;
+	}
+}
--- Assets/Scripts/Managers/EnemyManager.cs
+++ Assets/Scripts/Managers/EnemyManager.cs
@@ -6,6 +6,7 @@ public class EnemyManager : MonoBehaviour {
     public GameObject enemy;
     public float spawnTime = 3.0f;
     public Transform[] spawnPoints;
+	public EnemyCounter counter;
 
     void Start() {
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
@@ -14,10 +15,25 @@ public class EnemyManager : MonoBehaviour {
     void Spawn() {
         if (playerHealth.currentHealth <= 0f) {
             return;
-        }
-
-        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-    }
+		}
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+		if (enemy.gameObject.tag == "ZomBunny") {
+			if (counter.bunnyNumber < counter.maxBunnyNumber) {
+				counter.increaseBunny();
+				Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			}
+		}
+		if (enemy.gameObject.tag == "ZomBear") {
+			if (counter.bearNumber < counter.maxBearNumber) {
+				counter.increaseBear();
+				Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			}
+		}
+		if (enemy.gameObject.tag == "Hellephant") {
+			if (counter.elepNumber < counter.maxElepNumber) {
+				counter.increaseElep();
+				Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+			}
+		}
+	}
 }
