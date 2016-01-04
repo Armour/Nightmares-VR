b6f903e2776c4763d3481bf57a93f7560eaccd7b
Armour <497052684@qq.com>
2016-01-03 20:06:31 -0800

    added winScore

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOverManager : MonoBehaviour {
 
     public PlayerHealth playerHealth;
+	public float winScore = 300.0f;
 
     Animator anim;
+	private Text gameoverText;
 
     void Awake() {
         anim = GetComponent<Animator>();
+		gameoverText = GetComponentInChildren<Text>();
     }
 
     void Update() {
         if (playerHealth.currentHealth <= 0) {
+			gameoverText.text = "You Lose !";
             anim.SetTrigger("GameOver");
-        }
+		} else if (ScoreManager.score >= 300f) {
+			gameoverText.text = "You Win !";
+			anim.SetTrigger("GameOver");
+		}
 	}
 }
